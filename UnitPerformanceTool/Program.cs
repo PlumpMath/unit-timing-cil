6c641a3eb50ba4de07b0709672020760f13aed28
dmoonfire <dmoonfire@84af6989-d30b-0410-891c-c1f6a83ca0db>
2009-04-23 03:00:51 +0000

    Adding the unit performance tool and the beginning of the testing framework.
    git-svn-id: http://svn.mfgames.com/PerformanceUnit/trunk@984 84af6989-d30b-0410-891c-c1f6a83ca0db

---
+++ UnitPerformanceTool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UnitPerformanceTool
+{
+	/// <summary>
+	/// A simple tool for scanning DLL's and performing unit tests
+	/// on them.
+	/// </summary>
+	public class Program
+	{
+		/// <summary>
+		/// Indicates the main entry into the application.
+		/// </summary>
+		/// <param name="args">The args.</param>
+		static void Main(string[] args)
+		{
+			throw new Exception("Bob");
+		}
+	}
+}
